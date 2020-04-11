bf69fde84ce54efa1b09e0874af7e2be1758fb8b
Lance Bachmeier <lanceb@ksu.edu>
2020-04-11 15:22:24 -0500

    nm

--- share/jenny/layout/post-old.sh
+++ share/jenny/layout/post-old.sh
@@ -9,7 +9,6 @@ cat << _EOF_
       <title>$(echo $POST_TITLE)</title>
       <link href='https://fonts.googleapis.com/css?family=Roboto:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
       <link href='https://fonts.googleapis.com/css?family=Fira+Mono' rel='stylesheet' type='text/css'>
-      <link href="https://fonts.googleapis.com/css2?family=Lora&display=swap" rel="stylesheet"> 
       <link href="data:image/x-icon;base64,AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAANjY2AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAARERERAAAAAAAAAAAAAAAAEQEBEQAAAAAAAAAAAAAAABEREREAAAAAAAAAAAAAAAARAREBAAAAAAAAAAAAAAAAEREBEQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD//wAA//8AAP//AADwDwAA//8AAPKPAAD//wAA8A8AAP//AADyLwAA//8AAPCPAAD//wAA//8AAP//AAD//wAA" rel="icon" type="image/x-icon">
 <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
 <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
@@ -20,10 +19,8 @@ cat << _EOF_
           color: #444;
           font-size: 22px;
           padding: 1em;
-          font-family: "Lora", 'Roboto', sans-serif;
+          font-family: 'Roboto', sans-serif;
           line-height: 1.5em;
-          max-width: 760px; 
-          margin: auto;
         }
         @media (min-width: 736px) { body { font-size: 22px } }
         article { padding: 0; margin: 1em 0; }
@@ -33,21 +30,21 @@ cat << _EOF_
         article blockquote,
         article pre { background-color: #f4f4f4; margin: 0; padding: 1em; border-radius: 7px; border: 2px solid #DDD; }
         article pre,
-        article code { font-family: 'Ubuntu Mono, "Fira Mono", monospace'; font-size: 22px; color: #333; }
+        article code { font-family: 'Ubuntu Mono'; font-size: 22px; color: #333; }
         article pre { word-break: break-all; white-space: pre-wrap; }
         article code::first-line { line-height: 0 }
         article blockquote :first-of-type { margin-top: 0; }
         article blockquote :last-of-type { margin-bottom: 0; }
         article hr { border: 0; border-bottom: 3px solid #CCC; }
-        .heading { text-transform: uppercase; line-height: 1.6em; width: 400px; }
+        .heading { text-transform: uppercase; line-height: 1.6em; }
         .heading a { text-decoration: none; }
-        .heading .title { color: #222; display: inline-block; margin: 0 0 1em; font-weight:bold; font-family: Roboto;  }
+        .heading .title { color: #222; display: inline-block; margin: 0 0 1em; font-weight:bold  }
         .heading .title:hover { text-decoration: underline; }
         .heading .stamp { color: #999; }
         .heading .stamp,
-        .home { display: inline-block; width: 5.66em; text-align:left; margin-right: 6.5em; }
-        .home { text-decoration: none; margin-bottom: 1.5em; text-align: left;  color: #cb4b16; } .home:hover { color: #dc322f; }
-        .contents { display: inline-block; vertical-align: top; max-width: 760px; }
+        .home { display: inline-block; width: 5.66em; text-align:right; margin-right: 6.5em; }
+        .home { text-decoration: none; margin-bottom: 1.5em; text-align: right;  color: #cb4b16; } .home:hover { color: #dc322f; }
+        .contents { display: inline-block; max-width: 60ch; vertical-align: top; }
         .contents :first-child { margin-top: 0; }
         h1, h2, h3, h4, h5, h6 { font-size: 1em; font-weight: normal; text-transform: uppercase; margin: 2em 0 1em; }
         ol, ul { padding-left: 1em; }
@@ -59,18 +56,13 @@ cat << _EOF_
       </style>
     </head>
     <body>
-			<div class="heading">
-				<a href="$(echo $POST_URL)"></a><br>		
-			</div>
-			<div class="contents">
-				<article>
-					<div class="heading"><h1 class="title">$(echo $POST_TITLE)</h1></div>
-					$(echo "$POST_CONTENTS")
-					<span class="stamp">Last Update: $(echo $POST_DATE)</span>
-					<div class="tags">$(for i in $TAGS; do echo "<a href=\"/tag/$i\">$i</a>"; done;)</div>
-					<a href="../index.html" class="home">← Back</a>
-				</article>
-			<div>
+      <article>
+      <div class="heading"><a href="$(echo $POST_URL)"><span class="stamp">$(echo $POST_DATE)</span><h1 class="title">$(echo $POST_TITLE)</h1></a></div>
+        <a href="../index.html" class="home">←</a><div class="contents">
+        $(echo "$POST_CONTENTS")
+        <div class="tags">$(for i in $TAGS; do echo "<a href=\"/tag/$i\">$i</a>"; done;)</div>
+        </div>
+      </article>
     </body>
   </html>
 _EOF_
